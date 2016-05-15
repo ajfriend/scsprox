d96433a8859975c9076527880cd92c9ecc4c34e3
AJ Friend <ajfriend@gmail.com>
2016-05-14 18:37:33 -0700

    tested to make sure no memory leaks

---
+++ mem_test.py
@@ -0,0 +1,37 @@
+"""
+This script runs the proxer many times and queries the
+process memory to see if memory is leaking at all.
+"""
+
+import os
+import psutil
+from scs_prox.prox_obj import Prox
+from scs_prox.examples import example_rand
+
+def get_mem_MB():
+    process = psutil.Process(os.getpid())
+    return process.memory_info().rss/1e6
+
+print("Before example data: ", get_mem_MB())
+prob, x_vars, true_sol = example_rand(1000, 500)
+
+
+print("Before Prox obj: ", get_mem_MB())
+prox = Prox(prob, x_vars, verbose=False)
+
+print("Before prox.prox: ", get_mem_MB())
+x0 = prox.prox(verbose=False)
+
+print("Before loop: ", get_mem_MB())
+
+steps_to_check = 3
+for i in range(steps_to_check*10):
+    x0 = prox.prox(x0, verbose=False, max_iters=100)
+    if i % steps_to_check == 0:
+        print("i: ", get_mem_MB())
+        print(prox.info)
+
+
+# todo: check the runtimes on big problems. py vs C
+# periodically within this run
+        
